c16db9637f6cf2033a2252f0d1e45fb26af7f20e
Hilmasfa11 <hilmasitifadilahamalia@upi.edu>
2023-10-14 22:29:27 +0700

    update html & add js

---
+++ main.js
@@ -0,0 +1,49 @@
+// console.log("Hello world")
+// alert("ada notifikasi")
+// prompt("Pinjam Seratus")
+
+// var Promnet = "Coding is Easy"
+// console.log(Promnet)
+// var Promnet = "Coding is Easy"
+// console.log(Promnet)
+// var Promnet = "Coding is Easy"
+// console.log(Promnet)
+
+// let Promnet = "Coding is Fun"
+// console.log(Promnet)
+// let Promnet = "Coding is Fun"
+// console.log(Promnet)
+
+// const Promnet = "Coding is My Life"
+// console.log(Promnet)
+// const Promnet = "Coding is My Life"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Masukkan Poin Anda")
+// if(totalPoin > 100){
+//     document.write("Congratulation");
+//     }else{
+//         document.write("Keep Trying")
+// }
+
+// let x = 6;
+// let y = 3;
+
+// console.log(x<10 && y > 1);
+// console.log(x<10 && y < 1);
+// console.log(x==5||y==5);
+// console.log(x==6||y==5);
+// console.log(!(x==y));
+
+// document.write(x<10 && y > 1);
+// document.write("<br>");
+// document.write(x<10 && y < 1);
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
